f1cb1995a730e8f138814c360fd821348d3c3e0c
KAMAH <limitlesskds@gmail.com>
2023-12-24 10:01:35 +0100

    Added a readme file and commented out instance method calls

---
+++ ReadMe.md
@@ -0,0 +1,25 @@
+
+# Project Description
+
+alt-expense let's a user record financial expenses made. Boasting of two individual classes. One for handling the expenses made and another for saving or recording the expense to an in memory database. 
+
+alt-expense also has nice built in method with proper response messages that helps the user properly engagewith the project.
+
+# Clone
+
+To clone this project 
+* Using git:
+On your shell environment / bash type in ->     
+``` git clone https://github.com/KDS-LIMITLESS/alt-expense.git```
+
+* Using Github
+On your browser, navigate to ```https://github.com/KDS-LIMITLESS/alt-expense```   
+click on the green ```code``` tab then download the file as zip.     
+select a location on you computer to save the file. proceed to unzip the file aferwards.   
+Open up with your favourite code editor.
+
+
+# Usage
+
+To use the code. On your code editor, type in ```python alt-expense.py```.
+* All methods have been commented out further down in the file marked as ************Initialize Project ***********.*Feel free to uncomment(remove the leading # after the initialize Project)* and interact with the functionalities as needed. 
--- alt-expense.py
+++ alt-expense.py
@@ -30,18 +30,15 @@ class Expense:
   
 
 
-
+# Expense Database
 
 class ExpenseDatabase:
   def __init__(self) -> None:
     self.expenses = {}
 
-
-
   def add_expense(self, expense: Expense):
     self.expenses[expense.expense_id] = expense
     return expense.to_dict()
-
 
   def remove_expense(self, expense_id: int):
     expense: Expense
@@ -50,13 +47,9 @@ class ExpenseDatabase:
         return self.expenses.pop(expense.expense_id)
     return 'Expense not found'
   
-
-
   def get_expense(self, expense_id: int) -> Union[Expense, None]:
     return self.expenses.get(expense_id)
   
-
-
   def get_expense_by_title(self, expense_title: str):
     expense: Expense
     expense_list = []
@@ -64,8 +57,6 @@ class ExpenseDatabase:
       if expense.title == expense_title:
         expense_list.append(expense.to_dict())
     return expense_list 
-  
-
 
   def to_dict(self):
     expense_list: list[Expense] = []
@@ -75,10 +66,7 @@ class ExpenseDatabase:
 
 
 
-
-
-
-
+# ************Initialize Project ***********
 
 # Initialize expense_database
 expense_db = ExpenseDatabase()
@@ -90,3 +78,15 @@ expense_2 = Expense(2, "Testing 2", 4000)
 # Save expense data to database
 expense_db.add_expense(expense_1)
 expense_db.add_expense(expense_2)
+
+# Expense database methods 
+
+# print(expense_db.remove_expense(1))
+# print(expense_db.get_expense(2))
+# print(expense_db.get_expense_by_title("Testing Expense"))
+# print(expense_db.to_dict())
+
+# Expense class methods
+
+# print(expense_1.update("Updating expense 1", 3000))
+# print(expense_2.to_dict())
